c9fd1ba331400758724bd6c67fe3b4fe44656aad
Stian Langvann <stian.langvann@gmail.com>
2023-11-17 17:02:38 +0000

    Update capnum.py
    added class and signal handler

--- capnum.py
+++ capnum.py
@@ -1,51 +1,68 @@
 import asyncio
 import gi
+import signal
+import sys
+
 gi.require_version('Gtk', '3.0')
 gi.require_version('GtkLayerShell', '0.1')
 from gi.repository import Gtk, GtkLayerShell, GLib
 
-indicators = {
-    'numlock': '/sys/class/leds/input34::numlock/brightness',
-    'capslock': '/sys/class/leds/input34::capslock/brightness'
-}
+class VisKeys():
 
-states = {'numlock': None, 'capslock': None}
-window = None
-
-# Read in initial states
-for indicator in indicators:
-    states[indicator] = int(open(indicators[indicator], 'r').read().strip())
-
-
-async def main():
-    while True:
-        for indicator in indicators:
-            newstate = int(open(indicators[indicator], 'r').read().strip())
-            if newstate != states[indicator]:
-                states[indicator] = newstate
-                text = collate(indicator, newstate)
-                await asyncio.create_task(Overlay(text))
-        await asyncio.sleep(0.1)
+    def __init__(self):
+        self.indicators = {
+            'numlock': '/sys/class/leds/input21::numlock/brightness',
+            'capslock': '/sys/class/leds/input21::capslock/brightness'
+        }
+        self.states = {'numlock': None, 'capslock': None}
+        self.window = None
+        # Read in initial states
+        try:
+            for indicator in self.indicators:
+                self.states[indicator] = int(open(self.indicators[indicator], 'r').read().strip())
+        except FileNotFoundError:
+            print('Unable to locate the files set in the script. please update this part in the script:')
+            print(str(self.indicators))
+            sys.exit(1)
 
 
-async def Overlay(message):
-    window = Gtk.Window()
-    label = Gtk.Label(label=message)
-    window.add(label)
+    async def run(self) -> None:
+        while True:
+            for indicator in self.indicators:
+                newstate = int(open(self.indicators[indicator], 'r').read().strip())
+                if newstate != self.states[indicator]:
+                    self.states[indicator] = newstate
+                    text = "   " + indicator.upper() + " is "
+                    text += "ON " if newstate == 1 else "OFF "
+                    await asyncio.create_task(self.Overlay(text))
+            await asyncio.sleep(0.1)
 
-    GtkLayerShell.init_for_window(window)
-    GtkLayerShell.auto_exclusive_zone_enable(window)
 
-    window.show_all()
-    window.connect('destroy', Gtk.main_quit)
-    GLib.timeout_add_seconds(1, window.destroy)
-    Gtk.main()
+    async def Overlay(self, message) -> None:
+        ''' Uses Pythons GDK bindings to overlay the data on the screen'''
+        self.window = Gtk.Window()
+        label = Gtk.Label(label=message)
+        self.window.add(label)
 
-def collate(indicator, newstate) -> str:
-    text = "   " + indicator.upper() + " is "
-    text += "ON " if newstate == 1 else "OFF "
-    return text
+        GtkLayerShell.init_for_window(self.window)
+        GtkLayerShell.auto_exclusive_zone_enable(self.window)
+
+        self.window.show_all()
+        self.window.connect('destroy', Gtk.main_quit)
+        GLib.timeout_add_seconds(1, self.window.destroy)
+        Gtk.main()
+
+    def signal_handler(self, sig, frame) -> None:
+        ''' Handles kill signal..'''
+        try:
+            self.window.destroy()
+        except Exception:
+            pass
+        print('thnx bye.')
+        sys.exit(0)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    app = VisKeys()
+    signal.signal(signal.SIGINT, app.signal_handler)
+    asyncio.run(app.run())
